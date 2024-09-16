467bb8cfc02b2bdcbf81b519c0ea29b9554940f7
Keah Bareno <157820990+KeahB@users.noreply.github.com>
2024-09-16 15:33:54 +0800

    Add files via upload

---
+++ 2wice but nice/function.js
@@ -0,0 +1,9 @@
+
+  
+   // Toggle mobile menu visibility
+   document.getElementById('menu-btn').addEventListener('click', function() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+  });
+
+  
